4662d603bf715fc313a664906f31bf6aed1007bf
muhammadjonqilichev <muhammadjonqilichev09112002@gmail.com>
2022-10-15 11:26:46 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material"
+const App = ()=> {
+  return (
+    <div>
+      <Typography variant="h1">Hello Jiga</Typography>
+    </div>
+  )
+}
+
+export default App;
